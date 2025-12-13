6e5292a4747985bb9a89e8a6dfe9ca1f5e353b4f
clusterDuck.js <sreeadarsh4@gmail.com>
2025-12-13 19:41:13 +0530

    feat: Add gallery images to the Gallery component

--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 import './gallery.css'
 import Logo from '../../assets/logo.webp'
-import { LuArrowRight } from 'react-icons/lu'
+import IMG1 from '../../assets/gallery/WA0001.jpg'
+import IMG2 from '../../assets/gallery/WA0002.jpg'
+import IMG3 from '../../assets/gallery/WA0003.jpg'
+import IMG4 from '../../assets/gallery/WA0004.jpg'
+import IMG5 from '../../assets/gallery/WA0005.jpg'
+import IMG6 from '../../assets/gallery/WA0006.jpg'
+import IMG7 from '../../assets/gallery/WA0007.jpg'
+import IMG8 from '../../assets/gallery/WA0008.jpg'
+import IMG9 from '../../assets/gallery/WA0009.jpg'
+import IMG10 from '../../assets/gallery/WA00010.jpg'
+import IMG11 from '../../assets/gallery/WA00011.jpg'
+import IMG12 from '../../assets/gallery/WA00012.jpg'
+import IMG13 from '../../assets/gallery/WA00013.jpg'
+import IMG14 from '../../assets/gallery/WA00014.jpg'
+import IMG15 from '../../assets/gallery/WA00015.jpg'
+import IMG16 from '../../assets/gallery/WA00016.jpg'
+
+
 
 function Gallery() {
     return (
@@ -13,12 +30,22 @@ function Gallery() {
             </section>
             <section className="gallery">
                 <div className='gallery-container'>
-                    <img src="https://images.unsplash.com/photo-1500595046743-cd271d694d30?w=800&q=80" alt="" />
-                    <img src="https://images.unsplash.com/photo-1566140967404-b8b3932483f5?w=800&q=80" alt="" />
-                    <img src="https://images.unsplash.com/photo-1581091226825-a6a2a5aee158?w=800&q=80" alt="" />
-                    <img src="https://images.unsplash.com/photo-1628088062854-d1870b4553da?w=800&q=80" alt="" />
-                    <img src="https://images.unsplash.com/photo-1516467508483-a7212febe31a?w=800&q=80" alt="" />
-                    <img src="https://images.unsplash.com/photo-1559181567-c3190ca9959b?w=800&q=80" alt="" />
+                    <img src={IMG1} alt="" />
+                    <img src={IMG2} alt="" />
+                    <img src={IMG3} alt="" />
+                    <img src={IMG4} alt="" />
+                    <img src={IMG5} alt="" />
+                    <img src={IMG6} alt="" />
+                    <img src={IMG7} alt="" />
+                    <img src={IMG8} alt="" />
+                    <img src={IMG9} alt="" />
+                    <img src={IMG10} alt="" />
+                    <img src={IMG11} alt="" />
+                    <img src={IMG12} alt="" />
+                    <img src={IMG13} alt="" />
+                    <img src={IMG14} alt="" />
+                    <img src={IMG15} alt="" />
+                    <img src={IMG16} alt="" />
                 </div>
             </section>
         </div>
